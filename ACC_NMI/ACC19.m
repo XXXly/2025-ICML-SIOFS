7e474a0abb3030213de6e48c2ab90387c2400220
XXXly <509942100@qq.com>
2025-05-20 18:23:55 +0800

    Add files via upload

--- ACC_NMI/ACC19.m
+++ ACC_NMI/ACC19.m
@@ -1,13 +1,13 @@
-function N_acc = ACC6(aa, bb, cc, train, test, order)
+function N_acc = ACC19(aa, bb, cc, train, test, order)
 
-    tr3d = train(:,3:end);  te3d = test(:,3:end);  N_acc = zeros(1,6);
-    train_label = train(:,1);  test_label = test(:,1);
+    tr3d = train(:,3:end);  te3d = test(:,3:end);  N_acc = zeros(1,19);
+    train_label = train(:,1); test_label = test(:,1); d = size(train(:,3:end),2);
     if size(order,1) > size(order,2)
         order = order.';
     end
 
     trainn = zeros(size(tr3d));  testt = zeros(size(te3d));
-    sele_feature_N = aa: cc: bb;
+    sele_feature_N = round((aa: cc: bb)*d);
     for sele_N = 1: 1: length(sele_feature_N)
         sele_feature = order(1,1:sele_feature_N(sele_N));
         trainn(:,sele_feature) = tr3d(:,sele_feature);
---
+++ ACC_NMI/NMI6.m
@@ -0,0 +1,14 @@
+function nmi = NMI6(aa, bb, cc, train, feat_idx)
+
+	nmi = zeros(1,6);  train_inst = train(:,3:end);  train_label = train(:,1);
+    c = length(unique(train_label));
+    train_inst = NormalizeFea(train_inst);
+    sele_feature_N = aa: cc: bb;
+    for sele_N = 1: 1: length(sele_feature_N)
+        sele_feature = feat_idx(1,1:sele_feature_N(sele_N));
+        selected_inst = train_inst(:,sele_feature);
+        res = litekmeans_NoRan(selected_inst,c);
+        res = bestMap(train_label,res);
+        nmi(1,sele_N) = nmi(1,sele_N) + MutualInfoo(train_label,res);
+        clear sele_feature selected_inst res
+    end
